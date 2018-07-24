e5b3f99c05a58129ed7ea708f8c541d4451b26a5
AP\dazhan <Damon.Zhang@acxiom.com>
2018-07-24 17:07:28 +0800

    使用Scrapy框架爬取新浪微博-----薇姿，（关注，粉丝，微博）信息。

---
+++ weibo_weizi/weibo_weizi/items.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+
+class UserItem(Item):
+    collection = 'user'
+    id = Field()
+    name = Field()
+    avatar = Field()
+    cover = Field()
+    gender = Field()
+    description = Field()
+    fans_count = Field()
+    follows_count = Field()
+    weibo_count = Field()
+    verified = Field()
+    verified_reason = Field()
+    verified_type = Field()
+    crawled_at = Field()
+
+
+class UserRelationItem(Item):
+    collection = 'user_relation'
+    id = Field()
+    follows = Field()
+    fans = Field()
+
+
+class WeiboItem(Item):
+    collection = 'weibos'
+    id = Field()
+    attitudes_count = Field()
+    comments_count = Field()
+    reposts_count = Field()
+    picture = Field()
+    pictures = Field()
+    source = Field()
+    text = Field()
+    user = Field()
+    created_at = Field()
+    crawled_at = Field()
---
+++ weibo_weizi/weibo_weizi/pipelines.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from weibo_weizi.items import *
+import time, re
+import pymongo
+
+
+class TimePipeline(object):
+    def process_item(self, item, spider):
+        if isinstance(item, UserItem) or isinstance(item, WeiboItem):
+            now = time.strftime('%Y-%m-%d %H:%M', time.localtime())
+            item['crawled_at'] = now
+        return item
+
+
+class WeiboPipeline(object):
+    def parse_time(self, date):
+        if re.match('刚刚', date):
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
+        if re.match('\d+分钟前', date):
+            minute = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(minute) * 60))
+        if re.match('\d+小时前', date):
+            hour = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(hour) * 60 * 60))
+        if re.match('昨天.*', date):
+            day = re.match('昨天(.*)', date).group(1).strip()
+            date = time.strftime('%Y-%m-%d', time.localtime(time.time() - 24 * 60 * 60)) + ' ' + day
+        if re.match('\d{1,2}-\d{1,2}', date):
+            date = time.strftime('%Y-', time.localtime()) + date + ' 00:00'
+        return date
+
+    def process_item(self, item, spider):
+        if isinstance(item, WeiboItem):
+            if item.get('created_at'):
+                item['created_at'] = self.parse_time(item.get('created_at').strip())
+            if item.get('pictures'):
+                item['pictures'] = [pic.get('url') for pic in item.get('pictures')]
+        return item
+
+
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DB')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        self.db[UserItem.collection].create_index([('id', pymongo.ASCENDING)])
+        self.db[WeiboItem.collection].create_index([('id', pymongo.ASCENDING)])
+
+    def process_item(self, item, spider):
+        if isinstance(item, UserItem) or isinstance(item, WeiboItem):
+            self.db[item.collection].update({'id': item.get('id')}, {'$set': item}, True)
+        if isinstance(item, UserRelationItem):
+            self.db[item.collection].update({'id': item.get('id')}, {'$addToSet': {'follows': {'$each': item['follows']},'fans': {'$each': item['fans']}}}, True)
+        return item
+
+    def close_spider(self, spider):
+        self.client.close()
